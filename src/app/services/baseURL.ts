7547b0f0dfd23f145b5899b9dc087a0621440363
Ruslan <4ruslan.k@gmail.com>
2018-08-01 19:44:23 +0500

    baseurl fixed

--- src/app/services/baseURL.ts
+++ src/app/services/baseURL.ts
@@ -1,2 +1,2 @@
-export const baseURL = 'https://warm-tor-81501.herokuapp.com/';
-// export const baseURL = 'http://localhost:3000/';
+// export const baseURL = 'https://warm-tor-81501.herokuapp.com/';
+export const baseURL = '/';
